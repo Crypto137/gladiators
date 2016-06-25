2c09fe86a98c2306c3b6e06e27687e845126daff
Crypto137 <crypto40k@gmail.com>
2016-06-25 11:29:44 +0300

    fixed variable names in the MainGame class

--- GladiatorsGame/MainGame.cs
+++ GladiatorsGame/MainGame.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
     - [DONE] Implement skill unlock on levels 3, 5 and 7
     - [DONE] Implement skill scaling with strength
     - [DONE] Implement ending after level 100.
+    - [DONE] Fix variable names.
     - Implement additional skills (+ states)
     - Implement skill costs
     - Implement enemy scaling
@@ -28,9 +29,9 @@ namespace GladiatorsGame
 {
     class MainGame
     {
-        static Player Player1 = new Player();
-        static Enemy CurrentEnemy = new Enemy();
-        static Log CombatLog = new Log();
+        static Player player1 = new Player();
+        static Enemy currentEnemy = new Enemy();
+        static Log combatLog = new Log();
 
         static void Main()
         {
@@ -48,56 +49,56 @@ namespace GladiatorsGame
                 Console.ReadKey();
 
                 //In-Game
-                Player1.Initialize();
-                CombatLog.WriteLine("Welcome to The Arena! Fight for your live with the number keys.", ConsoleColor.Yellow);
+                player1.Initialize();
+                combatLog.WriteLine("Welcome to The Arena! Fight for your live with the number keys.", ConsoleColor.Yellow);
 
                 //Gameplay Session Loop
                 do
                 {
-                    CurrentEnemy.Generate();
-                    CombatLog.WriteLine("You are now fighting " + CurrentEnemy.GetName() + ".", ConsoleColor.Yellow);
+                    currentEnemy.Generate();
+                    combatLog.WriteLine("You are now fighting " + currentEnemy.GetName() + ".", ConsoleColor.Yellow);
 
                     do
                     {
-                        Player1.SetFinishedTurn(false);
+                        player1.SetFinishedTurn(false);
                         UpdateScreen();
 
                         //Get player input and use skill accordingly
                         switch (GetPlayerInput())
                         {
                             case "D1":
-                                if (Player1.GetUnlockedSkill()[0] == true)
+                                if (player1.GetUnlockedSkill()[0] == true)
                                 {
-                                    CombatLog.WriteLine(Player1.Skill_HeroicAssault(CurrentEnemy), ConsoleColor.Green);
-                                    Player1.SetFinishedTurn(true);
+                                    combatLog.WriteLine(player1.Skill_HeroicAssault(currentEnemy), ConsoleColor.Green);
+                                    player1.SetFinishedTurn(true);
                                 }
                                 break;
                             case "D2":
-                                if (Player1.GetUnlockedSkill()[1] == true)
+                                if (player1.GetUnlockedSkill()[1] == true)
                                 {
-                                    CombatLog.WriteLine(Player1.Skill_DesperateStrike(CurrentEnemy), ConsoleColor.Green);
-                                    Player1.SetFinishedTurn(true);
+                                    combatLog.WriteLine(player1.Skill_DesperateStrike(currentEnemy), ConsoleColor.Green);
+                                    player1.SetFinishedTurn(true);
                                 }
                                 break;
                             case "D3":
-                                if (Player1.GetUnlockedSkill()[2] == true)
+                                if (player1.GetUnlockedSkill()[2] == true)
                                 {
-                                    CombatLog.WriteLine(Player1.Skill_Bash(CurrentEnemy), ConsoleColor.Green);
-                                    Player1.SetFinishedTurn(true);
+                                    combatLog.WriteLine(player1.Skill_Bash(currentEnemy), ConsoleColor.Green);
+                                    player1.SetFinishedTurn(true);
                                 }
                                 break;
                             case "D4":
-                                if (Player1.GetUnlockedSkill()[3] == true)
+                                if (player1.GetUnlockedSkill()[3] == true)
                                 {
-                                    CombatLog.WriteLine(Player1.Skill_DirtThrow(CurrentEnemy), ConsoleColor.Green);
-                                    Player1.SetFinishedTurn(true);
+                                    combatLog.WriteLine(player1.Skill_DirtThrow(currentEnemy), ConsoleColor.Green);
+                                    player1.SetFinishedTurn(true);
                                 }
                                 break;
                             case "D5":
-                                if (Player1.GetUnlockedSkill()[4] == true)
+                                if (player1.GetUnlockedSkill()[4] == true)
                                 {
-                                    CombatLog.WriteLine(Player1.Skill_SavageCut(CurrentEnemy), ConsoleColor.Green);
-                                    Player1.SetFinishedTurn(true);
+                                    combatLog.WriteLine(player1.Skill_SavageCut(currentEnemy), ConsoleColor.Green);
+                                    player1.SetFinishedTurn(true);
                                 }
                                 break;
                         }
@@ -106,66 +107,66 @@ namespace GladiatorsGame
                         System.Threading.Thread.Sleep(250);
 
                         //Enemy reaction (attack or death)
-                        if (CurrentEnemy.GetHealth() <= 0)
+                        if (currentEnemy.GetHealth() <= 0)
                         {
-                            CombatLog.WriteLine(CurrentEnemy.GetName() + " falls breathless before you.", ConsoleColor.Yellow);
+                            combatLog.WriteLine(currentEnemy.GetName() + " falls breathless before you.", ConsoleColor.Yellow);
                         }
-                        else if (Player1.GetFinishedTurn() == true)
+                        else if (player1.GetFinishedTurn() == true)
                         {
-                            CombatLog.WriteLine(CurrentEnemy.Attack(Player1), ConsoleColor.Red);
+                            combatLog.WriteLine(currentEnemy.Attack(player1), ConsoleColor.Red);
                         }
-                    } while ((CurrentEnemy.GetHealth() >= 1) && (Player1.GetHealth() >= 1));
+                    } while ((currentEnemy.GetHealth() >= 1) && (player1.GetHealth() >= 1));
 
-                    if (Player1.GetLevel() == 100)
+                    if (player1.GetLevel() == 100)
                     {
-                        Player1.SetFinishedGame(true);
+                        player1.SetFinishedGame(true);
                         break;
                     }
 
                     //Level Up
-                    if (Player1.GetHealth() >= 1)
+                    if (player1.GetHealth() >= 1)
                     {
-                        CombatLog.WriteLine(Player1.LevelUp(), ConsoleColor.Cyan);
-                        CombatLog.WriteLine("Choose stat to level up:", ConsoleColor.Cyan);
-                        CombatLog.WriteLine("1. Strength    2. Agility    3. Vitality", ConsoleColor.Cyan);
+                        combatLog.WriteLine(player1.LevelUp(), ConsoleColor.Cyan);
+                        combatLog.WriteLine("Choose stat to level up:", ConsoleColor.Cyan);
+                        combatLog.WriteLine("1. Strength    2. Agility    3. Vitality", ConsoleColor.Cyan);
                         UpdateScreen();
 
-                        while (Player1.GetFinishedLevelUp() == false)
+                        while (player1.GetFinishedLevelUp() == false)
                         {
                             switch (GetPlayerInput())
                             {
                                 case "D1":
-                                    Player1.SetStrength(Player1.GetStrength() + 1);
-                                    Player1.SetFinishedLevelUp(true);
+                                    player1.SetStrength(player1.GetStrength() + 1);
+                                    player1.SetFinishedLevelUp(true);
                                     break;
                                 case "D2":
-                                    Player1.SetAgility(Player1.GetAgility() + 1);
-                                    Player1.SetFinishedLevelUp(true);
+                                    player1.SetAgility(player1.GetAgility() + 1);
+                                    player1.SetFinishedLevelUp(true);
                                     break;
                                 case "D3":
-                                    Player1.SetVitality(Player1.GetVitality() + 1);
-                                    Player1.SetFinishedLevelUp(true);
+                                    player1.SetVitality(player1.GetVitality() + 1);
+                                    player1.SetFinishedLevelUp(true);
                                     break;
                             }
                         }
 
-                        Player1.UpdateStats();
+                        player1.UpdateStats();
                     }
-                } while (Player1.GetHealth() >= 1);
+                } while (player1.GetHealth() >= 1);
 
                 //Game Over
-                if (Player1.GetFinishedGame() == true)
+                if (player1.GetFinishedGame() == true)
                 {
-                    CombatLog.WriteLine("You have finished level 100 and earned your freedom. Congratulations!", ConsoleColor.Cyan);
-                    CombatLog.WriteLine("Press any key to return to title screen.", ConsoleColor.Cyan);
+                    combatLog.WriteLine("You have finished level 100 and earned your freedom. Congratulations!", ConsoleColor.Cyan);
+                    combatLog.WriteLine("Press any key to return to title screen.", ConsoleColor.Cyan);
                 }
                 else
                 {
-                    CombatLog.WriteLine("You are dead. Press any key to return to title screen.", ConsoleColor.Yellow);
+                    combatLog.WriteLine("You are dead. Press any key to return to title screen.", ConsoleColor.Yellow);
                 }
                 UpdateScreen();
                 Console.ReadKey();
-                CombatLog.Clear();
+                combatLog.Clear();
             }
         }
 
@@ -174,22 +175,22 @@ namespace GladiatorsGame
             Console.Clear();
 
             UserInterface.DrawHorizontalLine();
-            UserInterface.DrawEnemyInfo(CurrentEnemy.GetName(), CurrentEnemy.GetHealth(), CurrentEnemy.GetMaxHealth(), CurrentEnemy.GetStun(), CurrentEnemy.GetBleed(), CurrentEnemy.GetDaze());
+            UserInterface.DrawEnemyInfo(currentEnemy.GetName(), currentEnemy.GetHealth(), currentEnemy.GetMaxHealth(), currentEnemy.GetStun(), currentEnemy.GetBleed(), currentEnemy.GetDaze());
             UserInterface.DrawHorizontalLine();
-            UserInterface.DrawCombatLog(CombatLog.GetLineText(), CombatLog.GetLineColor());
+            UserInterface.DrawCombatLog(combatLog.GetLineText(), combatLog.GetLineColor());
             UserInterface.DrawHorizontalLine();            
-            UserInterface.DrawPlayerInfo(Player1.GetName(), Player1.GetLevel(), Player1.GetStrength(), Player1.GetAgility(), Player1.GetVitality(), Player1.GetHealth(), Player1.GetMaxHealth(), Player1.GetEnergy(), Player1.GetMaxEnergy(), Player1.GetStun(), Player1.GetBleed(), Player1.GetDaze());
+            UserInterface.DrawPlayerInfo(player1.GetName(), player1.GetLevel(), player1.GetStrength(), player1.GetAgility(), player1.GetVitality(), player1.GetHealth(), player1.GetMaxHealth(), player1.GetEnergy(), player1.GetMaxEnergy(), player1.GetStun(), player1.GetBleed(), player1.GetDaze());
             UserInterface.DrawHorizontalLine();
-            UserInterface.DrawSkillBar(Player1.GetUnlockedSkill());
+            UserInterface.DrawSkillBar(player1.GetUnlockedSkill());
             UserInterface.DrawHorizontalLine();
         }
 
         static string GetPlayerInput()
         {
-            string PlayerInput;
-            PlayerInput = Console.ReadKey().Key.ToString();
+            string playerInput;
+            playerInput = Console.ReadKey().Key.ToString();
 
-            return PlayerInput;
+            return playerInput;
         }
     }
 }
